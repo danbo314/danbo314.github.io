ad178c9121cc51604c7983815a0d507eace5a621
Dan Bolivar <danbo314@gmail.com>
2015-02-19 20:55:12 -0500

    contact page v1 without form

---
+++ js/contact.js
@@ -0,0 +1,26 @@
+$(function () {
+
+});
+
+function sendMail(from_email, subject, message) {
+    $.ajax({
+        type: 'POST',
+        url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+        data: {
+            'key': 'YJw5lv4rpDWIWJx3v-XIKw',
+            'message': {
+                'from_email': from_email,
+                'to': [{
+                    'email': 'danbo314@gmail.com',
+                    'name': '',
+                    'type': ''
+                }],
+                'autotext': 'true',
+                'subject': subject,
+                'html': message
+            }
+        }
+    }).done(function(response) {
+        console.log(response); // if you're into that sorta thing
+    });
+}
