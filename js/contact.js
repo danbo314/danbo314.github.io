495a9414a200b04448a8c7960071fcb88180c28a
Dan Bolivar <danbo314@gmail.com>
2015-02-20 02:36:24 -0500

    add contact form v1

--- js/contact.js
+++ js/contact.js
@@ -9,27 +9,40 @@ $(function () {
         }
     });
 
+    $("#submitContactForm").click(function() {
+        var email = $("#form_email").text(),
+            name = $("#form_name").text(),
+            message = $("#form_message").text();
+
+        sendMail(email, name, message);
+    });
+
 });
 
-function sendMail(from_email, subject, message) {
+function sendMail(from_email, name, message) {
     $.ajax({
         type: 'POST',
         url: 'https://mandrillapp.com/api/1.0/messages/send.json',
         data: {
-            'key': 'YJw5lv4rpDWIWJx3v-XIKw',
-            'message': {
-                'from_email': from_email,
-                'to': [{
-                    'email': 'danbo314@gmail.com',
-                    'name': '',
-                    'type': ''
+            key: 'YJw5lv4rpDWIWJx3v-XIKw',
+            message: {
+                from_email: from_email,
+                from_name: name,
+                to: [{
+                    email: 'inquiry@danbolivar.me'
                 }],
-                'autotext': 'true',
-                'subject': subject,
-                'html': message
+                auto_text: 'true',
+                subject: "Inquiry from danbolivar.me",
+                html: message
             }
+        },
+        success: function (response, message, jqXHR) {
+            console.log(message);
+            console.log(response);
+        },
+        error: function (jqXHR, message, error) {
+            console.log(message);
+            console.log(error);
         }
-    }).done(function(response) {
-        console.log(response); // if you're into that sorta thing
     });
 }
